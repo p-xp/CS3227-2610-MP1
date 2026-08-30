ac500c9be8612741c240832e9ec2262937b80e00
XP <xp@XPs-MacBook-Air.local>
2026-08-30 22:53:31 +0800

    Rename Duke template to MeepMoop project

---
+++ src/main/java/MeepMoop.java
@@ -0,0 +1,15 @@
+/**
+ * The main entry point for the MeepMoop chatbot.
+ */
+public class MeepMoop {
+    public static void main(String[] args) {
+        String banner = " __  __                    __  __                  \n"
+                + "|  \\/  | ___  ___ _ __    |  \\/  | ___   ___  _ __ \n"
+                + "| |\\/| |/ _ \\/ _ \\ '_ \\   | |\\/| |/ _ \\ / _ \\| '_ \\\n"
+                + "| |  | |  __/  __/ |_) |  | |  | | (_) | (_) | |_) |\n"
+                + "|_|  |_|\\___|\\___| .__/   |_|  |_|\\___/ \\___/| .__/\n"
+                + "                 |_|                          |_|  \n"
+                + "              ~ Welcome to MeepMoop! ~\n";
+        System.out.println(banner);
+    }
+}
