4cbe89830b10558b723af87183031a6b77c0dede
XP <xp@XPs-MacBook-Air.local>
2026-08-30 23:00:59 +0800

    level0: greet and exit

--- src/main/java/MeepMoop.java
+++ src/main/java/MeepMoop.java
@@ -3,6 +3,8 @@
  */
 public class MeepMoop {
     public static void main(String[] args) {
+        String separator = "____________________________________________________________";
+
         String banner = " __  __                    __  __                  \n"
                 + "|  \\/  | ___  ___ _ __    |  \\/  | ___   ___  _ __ \n"
                 + "| |\\/| |/ _ \\/ _ \\ '_ \\   | |\\/| |/ _ \\ / _ \\| '_ \\\n"
@@ -10,6 +12,16 @@ public class MeepMoop {
                 + "|_|  |_|\\___|\\___| .__/   |_|  |_|\\___/ \\___/| .__/\n"
                 + "                 |_|                          |_|  \n"
                 + "              ~ Welcome to MeepMoop! ~\n";
+        
+        String welcomeMessage = "Hello! I'm MeepMoop. How can I assist you today?";
+        String goodbyeMessage = "Goodbye! Have a great day!";
+        
         System.out.println(banner);
+        System.out.println(separator);
+        System.out.println(welcomeMessage);
+        System.out.println(separator);
+        System.out.println(goodbyeMessage);
+        System.out.println(separator);
+
     }
 }
